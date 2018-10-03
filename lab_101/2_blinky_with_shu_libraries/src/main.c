ba8880c05722afb2c045ecde1989e99db7633287
zoorlop <b5046805@hallam.shu.ac.uk>
2018-10-03 16:24:55 +0100

    Working traffic lights

--- lab_101/2_blinky_with_shu_libraries/src/main.c
+++ lab_101/2_blinky_with_shu_libraries/src/main.c
@@ -19,7 +19,9 @@
 #include "gpio.h"
 
 // map the led to GPIO PI1 (again, this is the inbuilt led)
-gpio_pin_t led = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t led_red = {PI_1, GPIOI, GPIO_PIN_1};
+gpio_pin_t led_yellow = {PB_14, GPIOB, GPIO_PIN_14};
+gpio_pin_t led_green = {PB_15, GPIOB, GPIO_PIN_15};
 
 // this is the main method
 int main()
@@ -30,15 +32,36 @@ int main()
   init_sysclk_216MHz();
   
   // initialise the gpio pins
-  init_gpio(led, OUTPUT);
-  
+  init_gpio(led_red, OUTPUT);
+  init_gpio(led_yellow, OUTPUT);
+	init_gpio(led_green, OUTPUT);
+	
   // loop forever ...
   while(1)
   {
-    // toggle the led on the gpio pin
-    toggle_gpio(led);
-    
+    // red on, yellow off
+			write_gpio(led_red,HIGH);
+			write_gpio(led_yellow, LOW);
+    // wait for 4 second
+			HAL_Delay(4000);
+		
+		// yellow on 
+			write_gpio(led_yellow, HIGH);
     // wait for 1 second
-    HAL_Delay(1000);
+			HAL_Delay(1000);
+		
+		// red and yellow off, green on 
+			write_gpio(led_red, LOW);
+			write_gpio(led_yellow, LOW);
+			write_gpio(led_green, HIGH);
+    // wait for 4 seconds
+			HAL_Delay(4000);
+		
+		// yellow on, green off 
+			write_gpio(led_yellow, HIGH);
+			write_gpio(led_green, LOW);
+    // wait for 2 seconds
+			HAL_Delay(2000);	
+		
   }
 }
